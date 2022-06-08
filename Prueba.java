bd1c66e2af3f2e81c159e7bb329bb0a97f6c813f
estherhitos <esther.hitos@iescampanillas.com>
2022-06-08 19:24:37 +0200

    Primera Prueba de repaso  variables

---
+++ Prueba.java
@@ -0,0 +1,11 @@
+public class Prueba {
+    
+  public static void main(String[] args) {
+      String nombre = "Esther";
+      Integer seguidoresLinkeding = 200;
+      Integer seguidoresGit = 500;
+      String frase = "Hola me llamo "+nombre+" y tengo "+(seguidoresLinkeding+seguidoresGit)+" seguidores.";
+        System.out.println(frase);
+    }
+    
+}
