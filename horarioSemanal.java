7705e5dc250b78e3819d8cdb60884c2bfd866f96
estherhitos <esther.hitos@iescampanillas.com>
2022-06-13 19:03:53 +0200

    PROGRAMA HORARIOSEMANAL ACABADO

--- horarioSemanal.java
+++ horarioSemanal.java
@@ -1,12 +1,9 @@
 public class horarioSemanal{
+
     public static void main(String[] args) {
         int fila;
         int columna;
-        final String PROG = "\030[0m";
-        final String LM = "\031[7m";
-        final String BBDD = "\032[7m";
-        final String ED = "\033[7m";
-        final String SINF = "\034[7m";
+        
        
 
 
@@ -14,27 +11,33 @@ public class horarioSemanal{
         /*System.out.println("Introduce el numero de la asignatura, ");
         int horas = Integer.parseInt(System.console().readLine());*/
         /*String[][] horas = new String[6][5];*/
-        String[][] horas = {{"Lunes ","LM ","BBDD ","ED ","SINF "},
-                            {"PROG","LM","BBDD","ED","SINF"},
-                            {"PROG","LM","BBDD","ED","SINF"},
-                            {"PROG","LM","BBDD","ED","SINF"},
-                            {"PROG","LM","BBDD","ED","SINF"},
-                            {"PROG","LM","BBDD","ED","SINF"},
-                            {"PROG","LM","BBDD","ED","SINF"},};
-        System.out.println("de que asignatura quieres saber el horario");
+      
+        String[][] horas = { 
+                            {"PROG" ,"PROG" ,"LM" ,"BBDD", "FOL"},
+                            {"PROG","PROG","SINF","BBDD","FOL"},
+                            {"ED","PROG","SINF","BBDD","FOL"},
+                            {"LM","BBDD","SINF","PROG","SINF"},
+                            {"LM","BBDD","ED","PROG","SINF"},
+                            {"LM","BBDD","ED","PROG","SINF"}
+                        };
+        System.out.println("Que asignatura quieres saber el horario");
         String deseoUsuario=System.console().readLine();
-        for(fila = 0; fila < 6; fila++) {
+
+         
+        for(fila = 0; fila < 5; fila++) {
            
             for(columna = 0;columna <5; columna ++){
-
+                
                 if(deseoUsuario.equals(horas[fila][columna])){
-                System.out.print(horas[fila][columna]);
+                       
+                    System.out.printf("|%5s",horas[fila][columna]);
                 }else{
-                    System.out.print(" . ");
+                    System.out.printf("|%5s"," X ");
                 }
+               
             }
-            System.out.println(); 
+            System.out.println();
         }
-     
+         
     }
 } 
