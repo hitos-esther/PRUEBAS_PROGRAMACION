195115e85d39f4a4e0d9323d5a0e5023c65bcc3f
estherhitos <esther.hitos@iescampanillas.com>
2022-06-14 18:24:19 +0200

    repaso examen 15/06/2022

---
+++ NumeroPrimo.java
@@ -0,0 +1,13 @@
+/**
+ * Crear una fucion que me diga los primos
+ * y que recoga la funcion de una cadena de 1 al 100
+ * con un array y lo imprima por pantalla.
+ * 
+ * Muestra los números primos que hay entre 1 y 1000.
+ */
+
+public class NumeroPrimo {
+    public static void main(String[] args) {
+        
+    }
+}
