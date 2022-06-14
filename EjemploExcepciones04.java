52928ac12dbef00ed8610819b0fcf9fde3ac87a1
estherhitos <esther.hitos@iescampanillas.com>
2022-06-14 18:49:43 +0200

    2º

--- EjemploExcepciones04.java
+++ EjemploExcepciones04.java
@@ -2,15 +2,15 @@ import java.util.Scanner;
 public class EjemploExcepciones04 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        System.out.println("Este programa pinta varias l√≠neas de asteriscos");
-        System.out.print("Introduzca el n√∫mero total de asteriscos: ");
+        System.out.println("Este programa pinta varias lÌ≠neas de asteriscos");
+        System.out.print("Introduzca el numero total de asteriscos: ");
         int asteriscos = Integer.parseInt(s.nextLine());
-        System.out.print("Introduzca el n√∫mero de l√≠neas que quiere pintar: ");
+        System.out.print("Introduzca el numero de lÌ≠neas que quiere pintar: ");
         int lineas = Integer.parseInt(s.nextLine());
         int longitud = (asteriscos % lineas) == 0 ? asteriscos / lineas : (int)Math.ceil((double)asteriscos / lineas);
         int cuentaAsteriscos = 0;
     for (int i = 1; i <= lineas; i++) {
-        System.out.print("L√≠nea " + i + ": ");
+        System.out.print("LÌnea " + i + ": ");
         for (int j = 0; (j < longitud) && (cuentaAsteriscos++ < asteriscos); j++) {
         System.out.print("*");
         }
